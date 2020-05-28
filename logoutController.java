7e7037f33dd0c40ab970158fc95d47707fb1e9ed
Yujiang Liu <64232819+gino0721@users.noreply.github.com>
2020-05-29 08:32:34 +1000

    Create logoutController.java

---
+++ logoutController.java
@@ -0,0 +1,33 @@
+package com.missiontomars.controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class logoutController
+ */
+@WebServlet("/logoutController")
+public class logoutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public logoutController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().removeAttribute("loginUser");
+		response.sendRedirect("login.jsp");
+	}
+
+}
